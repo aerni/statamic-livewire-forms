7ad010fcabbe05c5d6ec5e792ce289d3dfcee278
Michael Aerni <hello@michaelaerni.ch>
2021-04-29 18:57:36 +0200

    Add widths to example fields view

--- resources/views/fields.blade.php
+++ resources/views/fields.blade.php
@@ -1,3 +1,14 @@
-@foreach ($fields as $field)
-    @include('statamic-livewire-forms::fields.' . $field->type)
-@endforeach
+<div class="grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-12">
+    @foreach ($fields as $field)
+        <div class="
+            {{ $field->width === 25 ? 'md:col-span-3' : '' }}
+            {{ $field->width === 33 ? 'md:col-span-4' : '' }}
+            {{ $field->width === 50 ? 'md:col-span-6' : '' }}
+            {{ $field->width === 66 ? 'md:col-span-8' : '' }}
+            {{ $field->width === 75 ? 'md:col-span-9' : '' }}
+            {{ $field->width === 100 ? 'md:col-span-12' : '' }}"
+        ">
+            @include('statamic-livewire-forms::fields.' . $field->type)
+        </div>
+    @endforeach
+</div>
