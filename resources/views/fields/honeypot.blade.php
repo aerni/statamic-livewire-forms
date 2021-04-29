bec32e6a6e53b1d8f96fef4e84f1bf542c36586c
Michael Aerni <hello@michaelaerni.ch>
2021-04-29 18:39:08 +0200

    Add tabindex and autocomplete to honeypot field

--- resources/views/fields/honeypot.blade.php
+++ resources/views/fields/honeypot.blade.php
@@ -4,5 +4,7 @@
         name="{{ $honeypot->handle }}"
         id="{{ $honeypot->handle }}"
         wire:model.defer="{{ $honeypot->key }}"
+        tabindex="-1"
+        autocomplete="off"
     />
 </div>
