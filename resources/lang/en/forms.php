05db150064b7d62ac81db3a70e67e3da1d9e4d2d
Michael Aerni <hello@michaelaerni.ch>
2021-05-01 12:29:32 +0200

    Add error translations

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -5,4 +5,7 @@ return [
     'submit_button_label' => 'Send',
     'success_message' => 'The form was successfully submitted!',
 
+    'has_error' => 'There was {{ errors:count }} error with your submission',
+    'has_errors' => 'There were {{ errors:count }} errors with your submission',
+
 ];
