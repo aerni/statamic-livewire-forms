25db143c3d6ed76fd7537616d993a6d3881596f7
Michael Aerni <hello@michaelaerni.ch>
2021-05-03 09:23:32 +0200

    Add option to include a single field

--- resources/views/blade/fields.blade.php
+++ resources/views/blade/fields.blade.php
@@ -1,4 +1,5 @@
-@foreach ($fields as $field)
+@if ($field)
+
     <div class="
         col-span-1
         {{ $field['width'] === 25 ? 'md:col-span-3' : '' }}
@@ -10,4 +11,21 @@
     ">
         @include('statamic-livewire-forms::fields.' . $field['type'])
     </div>
-@endforeach
+
+@else
+
+    @foreach ($fields as $field)
+        <div class="
+            col-span-1
+            {{ $field['width'] === 25 ? 'md:col-span-3' : '' }}
+            {{ $field['width'] === 33 ? 'md:col-span-4' : '' }}
+            {{ $field['width'] === 50 ? 'md:col-span-6' : '' }}
+            {{ $field['width'] === 66 ? 'md:col-span-8' : '' }}
+            {{ $field['width'] === 75 ? 'md:col-span-9' : '' }}
+            {{ $field['width'] === 100 ? 'md:col-span-12' : '' }}"
+        ">
+            @include('statamic-livewire-forms::fields.' . $field['type'])
+        </div>
+    @endforeach
+
+@endif
