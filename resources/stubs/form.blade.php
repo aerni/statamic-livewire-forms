293d46523ba0f883b9fc93c7ec4ff5fcdb664a18
Michael Aerni <hello@michaelaerni.ch>
2021-05-04 10:41:21 +0200

    Forgot to rename here

--- resources/stubs/form.blade.php
+++ resources/stubs/form.blade.php
@@ -1,9 +1,9 @@
 <form wire:submit.prevent="submit" class="w-full max-w-2xl">
     <div class="grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-12">
-        @include('statamic-livewire-forms::fields')
-        @include('statamic-livewire-forms::fields.submit')
-        @include('statamic-livewire-forms::fields.honeypot')
-        @include('statamic-livewire-forms::errors')
-        @include('statamic-livewire-forms::success')
+        @include('livewire-forms::fields')
+        @include('livewire-forms::fields.submit')
+        @include('livewire-forms::fields.honeypot')
+        @include('livewire-forms::errors')
+        @include('livewire-forms::success')
     </div>
 </form>
