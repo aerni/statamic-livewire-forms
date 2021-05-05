b9febb8bb551c2a364556b0eb46f21bb8cf52037
Michael Aerni <hello@michaelaerni.ch>
2021-05-05 16:08:12 +0200

    Betterify error messages

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -5,7 +5,7 @@ return [
     'submit_button_label' => 'Send',
     'success_message' => 'The form was successfully submitted!',
 
-    'error_message' => 'There was an error with your submission.',
-    'errors_message' => 'There were :count errors with your submission.',
+    'error_message' => 'There was an error with your submission:',
+    'errors_message' => 'There were :count errors with your submission:',
 
 ];
