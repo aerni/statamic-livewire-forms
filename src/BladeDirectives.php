12eaac572a11b2cf91034a92c496a4d574a73e7f
Michael Aerni <hello@michaelaerni.ch>
2021-10-20 10:30:52 +0200

    Add new field blade directive

--- src/BladeDirectives.php
+++ src/BladeDirectives.php
@@ -2,6 +2,7 @@
 
 namespace Aerni\LivewireForms;
 
+use Illuminate\Support\Facades\Blade;
 use Aerni\LivewireForms\Facades\Captcha;
 
 class BladeDirectives
@@ -12,5 +13,17 @@ class BladeDirectives
     public static function captchaKey(): string
     {
         return Captcha::key();
+    }
+
+    /**
+     * Get a single field by its handle.
+     */
+    public static function field(string $handle): string
+    {
+        return Blade::compileString("
+            @if (isset(\$fields))
+                @include('livewire-forms::field', ['field' => \$fields[$handle]])
+            @endif
+        ");
     }
 }
