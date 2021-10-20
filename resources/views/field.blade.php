3e2938ccea31ef2bee8fd193881b5c42bc1b94aa
Michael Aerni <hello@michaelaerni.ch>
2021-10-20 10:30:32 +0200

    Move field into its own partial

---
+++ resources/views/field.blade.php
@@ -0,0 +1,14 @@
+<div class="
+    col-span-1
+    {{ $field['width'] === 25 ? 'md:col-span-3' : '' }}
+    {{ $field['width'] === 33 ? 'md:col-span-4' : '' }}
+    {{ $field['width'] === 50 ? 'md:col-span-6' : '' }}
+    {{ $field['width'] === 66 ? 'md:col-span-8' : '' }}
+    {{ $field['width'] === 75 ? 'md:col-span-9' : '' }}
+    {{ $field['width'] === 100 ? 'md:col-span-12' : '' }}
+    @if ($field['type'] === 'honeypot')
+        hidden
+    @endif
+">
+    @include('livewire-forms::fields.' . $field['type'])
+</div>
