0830e7fdb6afd698149aee2a12c49a4ca2642e63
Michael Aerni <hello@michaelaerni.ch>
2021-12-23 15:03:45 +0100

    Update form view stub to use blade directives

--- resources/stubs/form.blade.php
+++ resources/stubs/form.blade.php
@@ -1,8 +1,8 @@
 <form wire:submit.prevent="submit" class="w-full max-w-2xl">
     <div class="grid grid-cols-1 gap-8 md:grid-cols-12">
-        @include('livewire-forms::fields')
-        @include('livewire-forms::submit')
-        @include('livewire-forms::errors')
-        @include('livewire-forms::success')
+        @formFields
+        @formSubmit
+        @formErrors
+        @formSuccess
     </div>
 </form>
