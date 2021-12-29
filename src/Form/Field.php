1cad4c257481c84d3c1d13fe9285e3c5790f3adc
Michael Aerni <hello@michaelaerni.ch>
2021-12-29 19:43:04 +0100

    Make it possible to change field config

--- src/Form/Field.php
+++ src/Form/Field.php
@@ -11,8 +11,24 @@ class Field
         //
     }
 
-    public function __get($name): mixed
+    public function __get(string $name): mixed
     {
         return Arr::get($this->config, $name);
+    }
+
+    public function __set(string $name, mixed $value): void
+    {
+        $this->config[$name] = $value;
+    }
+
+    public function config(array $config = null): array|self
+    {
+        if (! $config) {
+            return $this->config;
+        }
+
+        $this->config = $config;
+
+        return $this;
     }
 }
