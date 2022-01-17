a080e9c1010d5e51b9176ee8b4993394bd17613f
Michael Aerni <hello@michaelaerni.ch>
2022-01-17 20:20:05 +0100

    Better default handling for radios

--- src/Fields/Radio.php
+++ src/Fields/Radio.php
@@ -2,12 +2,13 @@
 
 namespace Aerni\LivewireForms\Fields;
 
+use Illuminate\Support\Arr;
 use Aerni\LivewireForms\Facades\Component;
-use Aerni\LivewireForms\Fields\Properties\WithCastBooleans;
 use Aerni\LivewireForms\Fields\Properties\WithInline;
-use Aerni\LivewireForms\Fields\Properties\WithInstructions;
 use Aerni\LivewireForms\Fields\Properties\WithOptions;
 use Aerni\LivewireForms\Fields\Properties\WithShowLabel;
+use Aerni\LivewireForms\Fields\Properties\WithCastBooleans;
+use Aerni\LivewireForms\Fields\Properties\WithInstructions;
 
 class Radio extends Field
 {
@@ -20,5 +21,19 @@ class Radio extends Field
     public function viewProperty(): string
     {
         return Component::getView('fields.radio');
+    }
+
+    public function defaultProperty(): ?string
+    {
+        $default = $this->field->defaultValue();
+        $options = $this->optionsProperty();
+
+        // Only get defaults values if they exist in the options.
+        $validDefaults = collect($options)->only($default);
+
+        // Get the first valid default and respect the order of the defaults.
+        return collect($default)->first(function ($value) use ($validDefaults) {
+            return $validDefaults->has($value);
+        });
     }
 }
