93cee5e1d1b14b963304016a003a0e990df28c1c
Michael Aerni <hello@michaelaerni.ch>
2022-01-17 19:27:55 +0100

    Better checkbox default value handling
    Remove default values if they don't exists in the options.

--- src/Fields/Checkbox.php
+++ src/Fields/Checkbox.php
@@ -3,11 +3,12 @@
 namespace Aerni\LivewireForms\Fields;
 
 use Aerni\LivewireForms\Facades\Component;
-use Aerni\LivewireForms\Fields\Properties\WithInline;
-use Aerni\LivewireForms\Fields\Properties\WithInstructions;
 use Aerni\LivewireForms\Fields\Properties\WithLabel;
+use Aerni\LivewireForms\Fields\Properties\WithInline;
 use Aerni\LivewireForms\Fields\Properties\WithOptions;
 use Aerni\LivewireForms\Fields\Properties\WithShowLabel;
+use Aerni\LivewireForms\Fields\Properties\WithInstructions;
+use Illuminate\Support\Arr;
 
 class Checkbox extends Field
 {
@@ -22,13 +23,11 @@ class Checkbox extends Field
         return Component::getView('fields.checkbox');
     }
 
-    public function defaultProperty(): array|string
+    public function defaultProperty(): array
     {
         $default = $this->field->defaultValue();
         $options = $this->optionsProperty();
 
-        return (count($options) > 1)
-            ? (array) $default
-            : array_first((array) $default);
+        return array_keys(Arr::only($options, $default));
     }
 }
