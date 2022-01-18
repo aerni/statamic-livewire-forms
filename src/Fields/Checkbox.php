a8c47ab4611d904d90918f50afe31647e1146821
Michael Aerni <hello@michaelaerni.ch>
2022-01-18 14:28:51 +0100

    Fix checkbox initial data

--- src/Fields/Checkbox.php
+++ src/Fields/Checkbox.php
@@ -22,12 +22,27 @@ class Checkbox extends Field
         return Component::getView('fields.checkbox');
     }
 
-    public function defaultProperty(): array
+    public function defaultProperty(): string|array|null
     {
         $default = $this->field->defaultValue();
         $options = $this->optionsProperty();
 
         // A default is only valid if it exists in the options.
-        return collect($options)->only($default ?? [])->keys()->toArray();
+        $default = collect($options)->only($default ?? [])->keys();
+
+        /**
+         * We want to save the submission data as an array if there is more than one option.
+         * To do this, we need to initialize the default data to an array.
+         * The array may contain valid defaults or be empty.
+         */
+        if (count($options) > 1) {
+            return $default->toArray();
+        }
+
+        /**
+         * If this is a single checkbox, we want to save the submission data as a string.
+         * We also want to initialize the data to the first default if there is one.
+         */
+        return $default->first();
     }
 }
