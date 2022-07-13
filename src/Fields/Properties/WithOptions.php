9c230938d0e01acb803018e55a5defe8d34016a6
Michael Aerni <hello@michaelaerni.ch>
2022-07-13 12:14:08 -0400

    Make it look prettier with arrow function

--- src/Fields/Properties/WithOptions.php
+++ src/Fields/Properties/WithOptions.php
@@ -7,8 +7,7 @@ trait WithOptions
     protected function optionsProperty(): array
     {
         return collect($this->field->get('options'))
-            ->mapWithKeys(function ($value, $key) {
-                return is_numeric($key) ? [$value => __($value)] : [$key => __($value)];
-            })->toArray();
+            ->mapWithKeys(fn ($value, $key) => is_numeric($key) ? [$value => __($value)] : [$key => __($value)])
+            ->toArray();
     }
 }
