701cbad55df51171808fd82eb3df804407eca4f3
Michael Aerni <hello@michaelaerni.ch>
2022-11-08 17:48:49 -0500

    Use field ID instead of handle

--- resources/stubs/theme/field.blade.php
+++ resources/stubs/theme/field.blade.php
@@ -1,15 +1,15 @@
 @if ($field->show)
 
-    <div wire:key="{{ $field->handle }}" class="
-        col-span-1
+    <div wire:key="{{ $field->id }}"
+        class="col-span-1
         {{ $field->width === 25 ? 'md:col-span-3' : '' }}
         {{ $field->width === 33 ? 'md:col-span-4' : '' }}
         {{ $field->width === 50 ? 'md:col-span-6' : '' }}
         {{ $field->width === 66 ? 'md:col-span-8' : '' }}
         {{ $field->width === 75 ? 'md:col-span-9' : '' }}
-        {{ $field->width === 100 ? 'md:col-span-12' : '' }}
-    ">
-        @include($field->view)
+        {{ $field->width === 100 ? 'md:col-span-12' : '' }}"
+    >
+            @include($field->view)
     </div>
 
 @endif
