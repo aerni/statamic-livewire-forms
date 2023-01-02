949ddfea8eebac3302c5572e884a9c275ed3aafa
Michael Aerni <hello@michaelaerni.ch>
2023-01-01 19:34:26 -0500

    Add HasStatamicForm trait

---
+++ src/Concerns/HasStatamicForm.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Aerni\LivewireForms\Concerns;
+
+use Statamic\Forms\Form;
+use Statamic\Facades\Site;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Statamic\Forms\SendEmails;
+use Statamic\Events\FormSubmitted;
+use Illuminate\Support\Facades\URL;
+use Statamic\Events\SubmissionCreated;
+use Statamic\Contracts\Forms\Submission;
+use Statamic\Exceptions\SilentFormFailureException;
+
+trait HasStatamicForm
+{
+    protected Submission $formSubmission;
+
+    public function submitForm(): self
+    {
+        return $this
+            ->validateForm()
+            ->makeFormSubmission()
+            ->handleFormEvents()
+            ->storeFormSubmission();
+    }
+
+    public function getFormProperty(): Form
+    {
+        return \Statamic\Facades\Form::find($this->formHandle)
+            ?? throw new \Exception("Form with handle [{$this->formHandle}] cannot be found.");
+    }
+
+    protected function validateForm(): self
+    {
+        $this->form->blueprint()->fields()
+            ->addValues($this->formData())
+            ->validate($this->formRules());
+
+        return $this;
+    }
+
+    protected function formRules(): array
+    {
+        if (Arr::isAssoc($this->formFields)) {
+            return collect($this->getRules())
+                ->only(array_keys($this->formFields))
+                ->mapWithKeys(fn ($rules, $field) => [$this->formFields[$field] => $rules])
+                ->all();
+        }
+
+        return collect($this->getRules())
+            ->only($this->formFields)
+            ->mapWithKeys(fn ($rules, $field) => [Str::afterLast($field, '.') => $rules])
+            ->all();
+    }
+
+    protected function formData(): array
+    {
+        if (Arr::isAssoc($this->formFields)) {
+            return collect($this->formFields)
+                ->mapWithKeys(fn ($field, $property) => [$field => $this->getPropertyValue($property)])
+                ->all();
+        }
+
+        return collect($this->formFields)
+            ->mapWithKeys(fn ($property) => [Str::afterLast($property, '.') => $this->getPropertyValue($property)])
+            ->all();
+    }
+
+    protected function makeFormSubmission(): self
+    {
+        $this->formSubmission = $this->form->makeSubmission();
+
+        // Add way to upload files.
+        // $assetIds = $this->formSubmission->uploadFiles($this->uploadedFiles());
+
+        // Should we normalize data? Or should this be up to the user?
+        // $values = array_merge($this->normalizedData(), $assetIds);
+
+        $processedData = $this->form->blueprint()->fields()
+            ->addValues($this->formData())
+            ->process()->values();
+
+        $this->formSubmission->data($processedData);
+
+        return $this;
+    }
+
+    protected function handleFormEvents(): self
+    {
+        $formSubmitted = FormSubmitted::dispatch($this->formSubmission);
+
+        if ($formSubmitted === false) {
+            throw new SilentFormFailureException();
+        }
+
+        SubmissionCreated::dispatch($this->formSubmission);
+
+        SendEmails::dispatch($this->formSubmission, Site::findByUrl(URL::previous()));
+
+        return $this;
+    }
+
+    protected function storeFormSubmission(): self
+    {
+        if ($this->form->store()) {
+            $this->formSubmission->save();
+        }
+
+        return $this;
+    }
+}
