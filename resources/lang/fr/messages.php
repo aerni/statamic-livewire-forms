7469d1acc650da483034863413e671931af9e368
FR6 <colt450+fh@gmail.com>
2023-02-03 13:44:42 -0500

    Added french translations (#29)

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'submit_button_label' => 'Soumettre',
+    'success_message' => 'Le formulaire a été envoyé avec succès!',
+    'error_message' => 'Il y a eu une erreur avec votre soumission:|Il y a :count erreurs avec votre soumission:',
+
+    'captcha_challenge' => 'Le défi reCAPTCHA n\'est pas valide.',
+
+];
