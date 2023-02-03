cde9ba3aec1dda8285726d082cd59920da5191d7
Michael Aerni <hello@michaelaerni.ch>
2023-02-03 14:17:01 -0500

    Autoload field views (#31)

--- src/Fields/Properties/WithView.php
+++ src/Fields/Properties/WithView.php
@@ -8,6 +8,14 @@ trait WithView
 {
     protected function viewProperty(): string
     {
-        return Component::getView("fields.{$this->field->get('view', static::$view)}");
+        $auto = Component::getView("fields.{$this->handle}");
+        $field = Component::getView("fields.{$this->field->get('view')}");
+        $default = Component::getView('fields.'.static::$view);
+
+        return match (true) {
+            view()->exists($field) => $field,
+            view()->exists($auto) => $auto,
+            default => $default
+        };
     }
 }
