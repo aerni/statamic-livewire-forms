a699a15511912a5e6ea5344867230da0e42406d3
Michael Aerni <hello@michaelaerni.ch>
2023-05-09 11:36:48 -0400

    Enable `always_save` for Hidden field

--- src/Fields/Hidden.php
+++ src/Fields/Hidden.php
@@ -6,6 +6,11 @@ class Hidden extends Field
 {
     protected static string $view = 'input';
 
+    protected function alwaysSaveProperty(): bool
+    {
+        return true;
+    }
+
     protected function showProperty(): bool
     {
         return false;
