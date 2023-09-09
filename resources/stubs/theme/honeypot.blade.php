0fd7215da1a256d7b720e32383c3a467bde1e9c1
Michael Aerni <hello@michaelaerni.ch>
2023-09-09 17:35:18 -0400

    Defer is now the default
    https://livewire.laravel.com/docs/upgrading#wiremodel

--- resources/stubs/theme/honeypot.blade.php
+++ resources/stubs/theme/honeypot.blade.php
@@ -8,7 +8,7 @@
         type="text"
         name="{{ $this->honeypot->id }}"
         id="{{ $this->honeypot->id }}"
-        wire:model.defer="{{ $this->honeypot->key }}"
+        wire:model="{{ $this->honeypot->key }}"
         tabindex="-1"
         autocomplete="off"
     />
