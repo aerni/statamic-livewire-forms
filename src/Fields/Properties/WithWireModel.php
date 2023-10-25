44de9c36db313c683c669e036500e5d01de058aa
Michael Aerni <hello@michaelaerni.ch>
2023-10-25 15:25:59 -0400

    Update fields on `change` event
    This partially reverts the changes of PR https://github.com/aerni/statamic-livewire-forms/pull/44.

---
+++ src/Fields/Properties/WithWireModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aerni\LivewireForms\Fields\Properties;
+
+trait WithWireModel
+{
+    protected function wireModelProperty(): ?string
+    {
+        $wireModel = $this->field->get('wire_model', 'change');
+
+        // "Defer" is Livewire's default so we don't want to return it as modifier.
+        return $wireModel !== 'defer' ? $wireModel : null;
+    }
+}
