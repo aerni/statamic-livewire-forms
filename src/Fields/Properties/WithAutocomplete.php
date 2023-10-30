a31303d9b82c559b8e4d3c03e674159a1650a481
Michael Aerni <hello@michaelaerni.ch>
2023-10-30 14:50:34 -0400

    Autocomplete option `on` doesn’t exists
    Returns null if no autocomplete has been configured for the field.

--- src/Fields/Properties/WithAutocomplete.php
+++ src/Fields/Properties/WithAutocomplete.php
@@ -4,8 +4,8 @@ namespace Aerni\LivewireForms\Fields\Properties;
 
 trait WithAutocomplete
 {
-    protected function autocompleteProperty(): string
+    protected function autocompleteProperty(): ?string
     {
-        return $this->field->get('autocomplete', 'on');
+        return $this->field->get('autocomplete');
     }
 }
