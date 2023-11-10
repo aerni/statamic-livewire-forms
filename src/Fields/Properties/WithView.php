b51d28204a1e0e0c34d1f69441f63bdaef824fe0
Michael Aerni <hello@michaelaerni.ch>
2023-11-10 18:06:06 +0100

    Fix loading of field views

--- src/Fields/Properties/WithView.php
+++ src/Fields/Properties/WithView.php
@@ -8,12 +8,12 @@ trait WithView
     {
         // Try to load a user-defined view first.
         if ($this->field->get('view')) {
-            return 'fields'.$this->field->get('view');
+            return 'fields.'.$this->field->get('view');
         }
 
         // Try to autoload the view by field handle.
         if (view()->exists("livewire-forms::fields.{$this->handle}")) {
-            return 'fields'.$this->handle;
+            return 'fields.'.$this->handle;
         }
 
         // Fall back to the default field view.
