5a42a715108dc68e265994f03ec5f5335c37bd5b
Michael Aerni <hello@michaelaerni.ch>
2023-12-16 10:03:32 -0500

    Fix hidden field not being hidden

--- src/Fields/Hidden.php
+++ src/Fields/Hidden.php
@@ -5,4 +5,9 @@ namespace Aerni\LivewireForms\Fields;
 class Hidden extends Field
 {
     protected string $view = 'default';
+
+    protected function hiddenProperty(): bool
+    {
+        return true;
+    }
 }
