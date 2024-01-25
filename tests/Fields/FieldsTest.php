69fa69473faf1be476b9a59a10ab9a359c729f3e
Michael Aerni <hello@michaelaerni.ch>
2024-01-24 20:27:51 -0500

    Add more tests

--- tests/Fields/FieldsTest.php
+++ tests/Fields/FieldsTest.php
@@ -1,18 +1,13 @@
 <?php
 
-use Livewire\Livewire;
-use Statamic\Fields\Field;
-use Statamic\Facades\Blueprint;
 use Aerni\LivewireForms\Fields\Text;
-use Aerni\LivewireForms\Livewire\Form;
+use Livewire\Livewire;
+use Statamic\Facades\Blueprint;
 use Statamic\Facades\Form as StatamicForm;
-use Aerni\LivewireForms\Livewire\DynamicForm;
 
 beforeEach(function () {
     Blueprint::makeFromFields([
-        'name' => ['type' => 'text', 'display' => 'Name'],
-        'email' => ['type' => 'text', 'display' => 'Email'],
-        'message' => ['type' => 'textarea', 'display' => 'Message']
+        'name' => ['type' => 'text'],
     ])->setHandle('contact')->setNamespace('forms')->save();
 
     StatamicForm::make('contact')->save();
@@ -31,6 +26,54 @@ afterEach(function () {
     StatamicForm::find('contact')->delete();
 });
 
-it('can get the id', function () {
+it('can set and get the value', function () {
+    $this->field->value('Michael');
+
+    expect($this->field->value())->toBe('Michael');
+});
+
+it('can reset the value', function () {
+    $this->field->value('Michael');
+    $this->field->resetValue();
+
+    expect($this->field->value())->toBe(null);
+});
+
+it('processed a property through its method', function () {
     expect($this->field->id)->toBe("{$this->component->getId()}-field-name");
+});
+
+it('caches a property when it is called', function () {
+    $this->field->handle();
+    $this->field->default;
+
+    expect(invade($this->field)->properties)->toEqual(['handle' => 'name', 'default' => null]);
+});
+
+it('can set and get an arbitary property', function () {
+    $this->field->foo('value');
+    $this->field->bar = 'value';
+
+    expect($this->field->foo)->toBe('value');
+    expect($this->field->bar())->toBe('value');
+});
+
+it('can set a property to null', function () {
+    $this->field->foo('value');
+
+    $this->field->foo(null);
+
+    expect($this->field->foo)->toBeNull();
+});
+
+it('can unset a property', function () {
+    $this->field->foo('value');
+
+    $this->field->unset('foo');
+
+    expect(array_keys($this->field->properties()))->not->toContain('foo');
+});
+
+it('can get all properties', function () {
+    expect($this->field->properties())->not->toBeEmpty();
 });
