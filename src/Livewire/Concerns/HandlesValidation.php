43a9466295765fe46c5e96456a63dca63aea041e
Michael Aerni <hello@michaelaerni.ch>
2024-04-15 10:23:01 -0400

    Allow using the `sometimes` rule

--- src/Livewire/Concerns/HandlesValidation.php
+++ src/Livewire/Concerns/HandlesValidation.php
@@ -4,6 +4,20 @@ namespace Aerni\LivewireForms\Livewire\Concerns;
 
 trait HandlesValidation
 {
+    public function bootHandlesValidation(): void
+    {
+        /**
+         * Remove all fields that are not submittable from the data before validation to replicate
+         * Statamic's suggested validation pattern: https://statamic.dev/conditional-fields#validation
+         * This allows us to conditionally apply validation to conditionally shown fields using the 'sometimes' rule.
+         */
+        $this->withValidator(function ($validator) {
+             collect($validator->getValue('fields'))
+                ->filter(fn ($value, $field) => $this->submittableFields[$field])
+                ->pipe(fn ($fields) => $validator->setValue('fields', $fields));
+        });
+    }
+
     protected function rules(): array
     {
         return $this->fields
