b2284ac8838968535a14592cad8eb13c5006f74c
Michael Aerni <hello@michaelaerni.ch>
2024-05-08 16:41:53 -0400

    Add type hint

--- src/Livewire/Concerns/WithSections.php
+++ src/Livewire/Concerns/WithSections.php
@@ -5,13 +5,14 @@ namespace Aerni\LivewireForms\Livewire\Concerns;
 use Aerni\LivewireForms\Form\Section;
 use Illuminate\Support\Collection;
 use Livewire\Attributes\Computed;
+use Statamic\Fields\Section as FormSection;
 
 trait WithSections
 {
     #[Computed]
     public function sections(): Collection
     {
-        return $this->formSections->map(function ($section, $index) {
+        return $this->formSections->map(function (FormSection $section, int $index) {
             return new Section(
                 number: $index + 1,
                 fields: $this->fields->intersectByKeys($section->fields()->all()),
@@ -23,6 +24,6 @@ trait WithSections
 
     public function section(string $handle): ?Section
     {
-        return $this->sections->firstWhere(fn ($section) => $section->handle() === $handle);
+        return $this->sections->firstWhere(fn (Section $section) => $section->handle() === $handle);
     }
 }
