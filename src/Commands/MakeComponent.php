e7f00d47fea03c7c41bb3ed0ae1067f79049e092
aerni <aerni@users.noreply.github.com>
2024-05-11 20:16:25 +0000

    Fix styling

--- src/Commands/MakeComponent.php
+++ src/Commands/MakeComponent.php
@@ -5,12 +5,13 @@ namespace Aerni\LivewireForms\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Statamic\Console\RunsInPlease;
+use Statamic\Facades\Form;
+
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\error;
 use function Laravel\Prompts\info;
 use function Laravel\Prompts\select;
-use Statamic\Console\RunsInPlease;
-use Statamic\Facades\Form;
 
 class MakeComponent extends Command
 {
