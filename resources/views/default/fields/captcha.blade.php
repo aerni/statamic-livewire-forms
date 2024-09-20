ea06251b280584b71adf8fbc77d9d001a079e0ee
Michael Aerni <hello@michaelaerni.ch>
2024-09-20 09:21:08 -0400

    Make reCAPTCHA work with lazy form components (#69)

--- resources/views/default/fields/captcha.blade.php
+++ resources/views/default/fields/captcha.blade.php
@@ -5,11 +5,12 @@
 @endif
 
 <div
+    x-data="grecaptcha({
+        field: '{{ $field->key }}',
+        siteKey: '@captchaKey',
+    })"
     id="{{ $field->id }}"
     class="g-recaptcha"
-    data-sitekey="@captchaKey"
-    data-callback="setResponseToken_{{ $this->getId() }}"
-    data-expired-callback="resetResponseToken_{{ $this->getId() }}"
     wire:ignore
     aria-label="{{ $field->id }}-label"
     @if($field->instructions)
@@ -24,15 +25,8 @@
 @endif
 
 @assets
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script>
+        window.grecaptchaOnloadCallback = () => window.grecaptchaIsReady = true
+    </script>
+    <script async defer src="https://www.google.com/recaptcha/api.js?onload=grecaptchaOnloadCallback&render=explicit"></script>
 @endassets
-
-<script>
-    function setResponseToken_{{ $this->getId() }}(token) {
-        @this.set('{{ $field->key }}', token)
-    }
-
-    function resetResponseToken_{{ $this->getId() }}() {
-        @this.set('{{ $field->key }}', null)
-    }
-</script>
