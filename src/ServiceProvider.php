94932a8a325d385f3ba1fa7ecd0224b370dd3617
Michael Aerni <hello@michaelaerni.ch>
2025-03-05 12:13:40 -0500

    Fix tests

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -28,14 +28,14 @@ class ServiceProvider extends AddonServiceProvider
     ];
 
     protected $scripts = [
-        __DIR__.'/../dist/js/livewire-forms.js',
-        __DIR__.'/../dist/js/form.js',
-        __DIR__.'/../dist/js/filepond.js',
-        __DIR__.'/../dist/js/grecaptcha.js',
+        __DIR__.'/../resources/dist/js/livewire-forms.js',
+        __DIR__.'/../resources/dist/js/form.js',
+        __DIR__.'/../resources/dist/js/filepond.js',
+        __DIR__.'/../resources/dist/js/grecaptcha.js',
     ];
 
     protected $stylesheets = [
-        __DIR__.'/../dist/css/filepond.css',
+        __DIR__.'/../resources/dist/css/filepond.css',
     ];
 
     public function bootAddon()
