ea32d04f88af653947f6d81e6eff123f14fb77c8
Michael Aerni <hello@michaelaerni.ch>
2026-03-13 11:04:57 +0100

    Restore wire:model behavior (#115)

--- src/Fields/Properties/WithWireModel.php
+++ src/Fields/Properties/WithWireModel.php
@@ -6,7 +6,7 @@ trait WithWireModel
 {
     protected function wireModelProperty(?string $wireModel = null): ?string
     {
-        $wireModel = $wireModel ?? $this->field->get('wire_model', 'change');
+        $wireModel = $wireModel ?? $this->field->get('wire_model', 'live.change');
 
         /* "Defer" is Livewire's default so we don't want to return it as modifier. */
         return $wireModel !== 'defer' ? $wireModel : null;
